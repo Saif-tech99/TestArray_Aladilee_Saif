1195653cb3ce0e926c0b407d1895e0849efca1c3
Saif Al-adilee <saifa9697@gmail.com>
2020-11-19 09:50:56 +0100

    zz

--- TestArray_Aladilee_Saif/Program.cs
+++ TestArray_Aladilee_Saif/Program.cs
@@ -8,19 +8,21 @@ namespace TestArray_Aladilee_Saif
         static void Main(string[] args)
         {
             int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
-            int[] arr2 = new int[] { 6, 7, 8, 9, 10 };
+            int[] arr2 = { 6, 7, 8, 9, 10 };
 
             for (int i = 0; i < arr1.Length; i++)
             {
                 arr1[i] = arr2[i];
-                Console.WriteLine($"{arr1[i]}");
+                Console.Write($"{arr1[i]}");
             }
             Console.WriteLine();
             for (int j = 0; j < arr2.Length; j++)
             {
-                arr2[j] = arr1[j];
-                Console.WriteLine($"{arr2[j]}");
+                arr2[j] = j;
+                Console.Write($"{arr2[j]}");
             }
+            Console.WriteLine();
+           
         }      
     }
 }
